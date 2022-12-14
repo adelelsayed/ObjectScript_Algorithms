b4ac19d0f284136024954fc432a3d14cf58d9f1e
Adel Elsayed <adelelsayed1991@gmail.com>
2022-12-14 07:17:31 +0400

    adding merge sort

---
+++ sorting/mergeSort.py
@@ -0,0 +1,62 @@
+#uses python3
+import sys
+# Python program for implementation of MergeSort
+def mergeSort(arr):
+    if len(arr) > 1:
+        # Finding the mid of the array
+        mid = len(arr)//2
+        # Dividing the array elements
+        L = arr[:mid]
+        # into 2 halves
+        R = arr[mid:]
+        # Sorting the first half
+        mergeSort(L)
+        # Sorting the second half
+        mergeSort(R)
+        i = j = k = 0
+        
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                
+                arr[k] = L[i]
+                
+                i += 1
+                
+            else:
+                arr[k] = R[j]
+                no_of_inversions[0]=no_of_inversions[0]+(len(L)-i)
+                j += 1
+            
+            
+            
+            k += 1
+            
+            
+        # Checking if any element was left
+        while i < len(L):
+            
+            arr[k] = L[i]
+            
+            i += 1
+            k += 1
+            
+        while j < len(R):
+            arr[k] = R[j]
+            
+            j += 1
+            k += 1
+        
+            
+        
+
+# Driver Code
+if __name__ == '__main__':
+    input = input()
+    n, *a = list(map(int, input.split()))
+    b = n * [0]
+    no_of_inversions=[0]
+    originalArr=[r for r in a]
+    mergeSort(a)
+    print("sorted: ", str(a))
+    print(no_of_inversions[0])
