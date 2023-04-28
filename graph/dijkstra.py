e9f79d9744bac7cd130f8c72cc376be916185f45
Adel Elsayed <adelelsayed1991@gmail.com>
2023-04-28 07:55:56 +0400

    updating dikstra algorithm in python

--- graph/dijkstra.py
+++ graph/dijkstra.py
@@ -13,10 +13,11 @@ import math
 
 
 def dijkstra(adj, startNode):
+    # initialize all distances to infinity and previous to None as all nodes never explored
     distance=[math.inf] * len(adj)
     previous=[None]*len(adj)
     distance[startNode]=0
-    priorityQueue=[(0,startNode)] #distance values, node idx
+    priorityQueue=[(0,startNode)] #distance values, node idx, priority queue serves as base for the loop to explore nodes based on node with shortest distance found so far
     while priorityQueue:
         priorityQueue.sort(key=lambda x:x[0]) #sort queue so 0th element will be the min distance node
         currentNode=priorityQueue[0]
@@ -24,8 +25,9 @@ def dijkstra(adj, startNode):
         currentNodeDistance=currentNode[0]
         for node in range(len(adj[currentNodeIdx])):
             nodeValue=adj[currentNodeIdx][node]
-            if not nodeValue: continue
+            if not nodeValue: continue #skipping 0 distance
             if distance[node]>distance[currentNodeIdx]+nodeValue :
+                # if new distance found through currentNode is shorter, update node to it
                 newVal=distance[currentNodeIdx]+nodeValue
                 distance[node]=newVal
                 previous[node]=currentNodeIdx
